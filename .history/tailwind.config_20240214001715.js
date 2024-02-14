fcda1dd6df6b8bd23aca5763f3896b2f90e7b61c
SaadYasin <qatar.horizon2009@gmail.com>
2024-02-14 17:15:36 +0300

    Third commit

---
+++ .history/tailwind.config_20240214001715.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bodyBg: "#2c2446",
+        primary: "#2c3b3f",
+        secondary: "#32434b",
+        textColor: "#ffffff",
+        equalBtn: "#d61b61",
+        divLines: "#3b4c54",
+        buttonBg: "#46555D",
+        operatorBg: "#f2574c",
+        signsBg: "#eb5834",
+      },
+    },
+  },
+  plugins: [],
+};
+
---
+++ .history/tailwind.config_20240214003736.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        bodyBg: "#2c2446",
+        primary: "#2c3b3f",
+        secondary: "#336467",
+        textColor: "#ffffff",
+        equalBtn: "#d61b61",
+        divLines: "#4f7689",
+        divLines2: "#27383f",
+        buttonBg: "#46555D",
+        operatorBg: "#f2574c",
+        signsBg: "#eb5834",
+      },
+    },
+  },
+  plugins: [],
+};
+
